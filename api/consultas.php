1bd50c57c5ab4afab9d5b3abf1cdb32d5fec41ba
jessra <89423609+jessra@users.noreply.github.com>
2024-01-11 16:10:48 -0400

    agregado caso y funcion de consulta de perfil

--- api/consultas.php
+++ api/consultas.php
@@ -12,8 +12,12 @@ ini_set('display_errors', '1');
 }
 require_once "../libs/db_fc.php";
 require_once "../funciones/funciones.php";
+$id = $_SESSION['id'];
 extract($_GET);
 switch($s) {
+	case 'perfil':
+		$data = perfil($db,$id);
+		break;
 	case "salir":
 		session_destroy();
 		setcookie("sid", "", time() - 3600,parse_url($_SERVER['HTTP_REFERER'], PHP_URL_PATH),parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST),false,true);
--- funciones/funciones.php
+++ funciones/funciones.php
@@ -1,3 +1,13 @@
 <?php
-
+  function perfil($db, $id) {
+    $sql = "SELECT id_person, nom_per, ape_per, ced_per, ema_per, dir_per, tel_per, nom_car ".
+    "FROM personas, personal, cargos WHERE id_per_person=id_per AND id_car_person=id_car AND ".
+    "id_person='$id'";
+    $res = $db->query($sql);
+    $data = array();
+    while ($r = $res->fetch_array(MYSQLI_ASSOC)) {
+      $data[] = $r;
+    }
+    return $data;
+  }
 ?>
